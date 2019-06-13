bc5e65222975987df515198dbe07f3abc4a5a721
Romulo Nascimento <romulo.nascimento@hotmart.com>
2019-06-13 13:28:30 -0300

    Refine songs response

--- src/controllers/Song.js
+++ src/controllers/Song.js
@@ -1,20 +1,36 @@
 import Spotify from '../services/Spotify';
 import Musixmatch from '../services/Musixmatch';
 
+const filterSpotifySongData = song => {
+    const { id, name, duration_ms, preview_url } = song;
+    const album = {
+        name: song.album.name,
+        release_date: song.album.release_date,
+        image_url: song.album.images[0].url
+    };
+    const artists = song.artists.map(artist => ({
+        id: artist.id,
+        name: artist.name 
+    }));
+    const isrc = song.external_ids.isrc;
+    return { id, name, duration_ms, preview_url, album, artists, isrc };
+};
+
 const Song = {
     search: (req, res) => {
         const query = req.query.q;
         const page = req.query.page || 0;
         const size = req.query.size || 10;
         Spotify.search(query, page, size).then(response => {
-            res.status(200).send(response.data);
+            const songs = response.data.tracks.items.map(song => filterSpotifySongData(song));
+            res.status(200).send(songs);
         }).catch(error => {
             res.status(500).send(error);
         });
     },
     get: (req, res) => {
         Spotify.song(req.params.id).then(response => {
-            res.status(200).send(response.data);
+            res.status(200).send(filterSpotifySongData(response.data));
         }).catch(error => {
             res.status(500).send(error);
         });
@@ -22,7 +38,7 @@ const Song = {
     lyrics: (req, res) => {
         Spotify.song(req.params.id).then(response => {
             Musixmatch.lyrics(response.data.external_ids.isrc).then(response => {
-                res.status(200).send(response);
+                res.status(200).send(response.lyrics);
             }).catch(error => {
                 res.status(500).send(error);
             });
